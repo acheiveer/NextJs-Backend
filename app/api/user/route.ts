b04a532dbfd49cc0a156eab1c5253ecf7b1a24fc
prabhakarsinghssm1@gmail.com <prabhakarsinghssm1@gmail.com>
2024-08-17 01:03:24 +0530

    created get and post route in next

---
+++ app/api/user/route.ts
@@ -0,0 +1,13 @@
+
+export function GET(){
+    return Response.json({
+        email:"singh@gamil.com",
+        name:"Prabhakar"
+    })
+}
+export function POST(){
+    return Response.json({
+        email:"singh@gamil.com",
+        name:"Prabhakar"
+    })
+}
